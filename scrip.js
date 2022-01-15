f9aa770485ff576a7e4f4986490283afe4ed9013
joaoEmanuel6 <joaoemanuel689@gmail.com>
2022-01-15 10:42:42 -0400

    Desafio 01 - Concluído

---
+++ scrip.js
@@ -0,0 +1,11 @@
+
+
+let nome = "João"
+let number = 18
+let anoAtual = 2022 - number
+let string = "Estou mexendo no console.log"
+const quartaFeira = false
+
+console.log("O resultado da Subtração é: " + anoAtual)
+console.log (string)
+console.log(typeof quartaFeira)
